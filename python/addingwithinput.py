89e46db2298951b2fe18f9e409a3a67885261188
Daniel Jr <89317269+goldenRicecakecoder@users.noreply.github.com>
2024-04-05 07:16:02 -0500

    Add files via upload

---
+++ python/addingwithinput.py
@@ -0,0 +1,9 @@
+#Adding Two numbers with user inputs
+num1 = input('Enter the first number : ')
+num2 = input('Enter the second number : ')
+
+#Add two numbers
+sum = float(num1) + float(num2)
+
+#Display the sum
+print('The sume of {0} and {1} is {2}'.format(num1, num2, sum))
